fc7f9bcc2f49f0fe6393ad89442d10a40757b655
Logan Compton <lcompto@iu.edu>
2018-06-26 09:54:44 -0400

    First part of homework 1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('It works!')
+const button = document.querySelector('button')
+
+const updateText = function() {
+    const pillow = document.querySelector('#topHeading')
+    pillow.textContent = 'Scott\'s Tots'
+}
+
+button.onclick = updateText
