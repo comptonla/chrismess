48883606bbfa49d6a50cc93bc0ab056c99c046ee
Logan Compton <lcompto@iu.edu>
2018-06-27 16:00:56 -0400

    Add renderItem class to JS file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form#flickForm')
 
+//get span
 const renderProperty = function(name, value) {
   const span = document.createElement('span')
   span.classList.add(name)
@@ -7,25 +8,37 @@ const renderProperty = function(name, value) {
   return span
 }
 
+const renderItem = function(flick) {
+  const item = document.createElement('li')
+  item.classList.add('flick')
 
+  //list of properties
+  const properties = Object.keys(flick)
+  
+  properties.forEach(function(propertyName) {
+    const span = renderProperty(propertyName, flick[propertyName])
+    item.appendChild(span)
+  })
+
+  return item
+}
+
+//function to handle submission
 const submitHandler = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
-  const flickSpan = renderProperty('name', f.flickName.value)
-  const chrisSpan = renderProperty('chris', f.whichChris.value)
-
-  const item = document.createElement('li')
-  item.classList.add('flick')
-  item.appendChild(flickSpan)
-  item.appendChild(chrisSpan)
+  const flick = {
+    name: f.flickName.value,
+    chris: f.whichChris.value,
+  }
   
+  const item = renderItem(flick)
+
   const list = document.querySelector('#flicks')
   list.appendChild(item)
 
   f.reset()
 }
-
-
 
 form.addEventListener('submit', submitHandler)
