a75661fc3caca6390eba836e7c839f005dc06aac
Logan Compton <lcompto@iu.edu>
2018-06-27 14:28:42 -0400

    Clean up output when form is submitted

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const form = document.querySelector('form#flickForm')
 
-const flickForm = function(ev) {
+const submitHandler = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
   const flickName = f.flickName.value
+  const flickSpan  = document.createElement('span')
+  flickSpan.classList.add('movieName')
+  flickSpan.textContent = flickName
+
+  const whichChris = f.whichChris.value
+  const chrisSpan = document.createElement('span')
+  chrisSpan.classList.add('chris')
+  chrisSpan.textContent = whichChris
+
   const item = document.createElement('li')
-  item.textContent = flickName 
+  item.classList.add('flick')
+  item.appendChild(flickSpan)
+  item.appendChild(chrisSpan)
   
   const list = document.querySelector('#flicks')
   list.appendChild(item)
@@ -28,4 +39,4 @@ const chrisForm = function(event) {
   tar.reset()
 }
 
-form.addEventListener('submit', flickForm)
+form.addEventListener('submit', submitHandler)
