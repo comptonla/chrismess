2b51cd0c7afa200c81d613d145e2aea08a8442e1
Logan Compton <lcompto@iu.edu>
2018-06-27 11:33:51 -0400

    Add a second field to the form and append to the list

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const form = document.querySelector('form#flickForm')
 
-const updateHeading = function(ev) {
+const flickForm = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
   const flickName = f.flickName.value
   const item = document.createElement('li')
-  item.textContent = flickName
+  item.textContent = flickName 
+
+  const whichChris = f.whichChris.value
+  const chris = document.createElement('li')
+  chris.textContent = whichChris
+  
 
   const list = document.querySelector('#flicks')
   list.appendChild(item)
+  list.appendChild(chris)
 
   f.reset()
 }
 
-form.addEventListener('submit', updateHeading)
+form.addEventListener('submit', flickForm)
