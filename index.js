d3f866736ed5b2346df5ea79d19ff21548458127
Logan Compton <lcompto@iu.edu>
2018-06-28 15:23:06 -0400

    Add delete and favorite buttons to page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 class App {
   constructor() {
+    this.flicks = []
+    this.list = document.querySelector('#flicks')
+
     const form = document.querySelector('form#flickForm')
-    this.array = []
     form.addEventListener('submit', (ev) => {
       ev.preventDefault()
       this.handleSubmit(ev)
@@ -15,18 +17,60 @@ class App {
     return span
   }
 
-  renderItem(flick) {
-    const item = document.createElement('li')
-    item.classList.add('flick')
+  renderActionButtons(flick, item) {
+    const actions = document.createElement('div')
+    actions.classList.add('actions')
+
+    const delButton = document.createElement('button')
+    delButton.classList.add('remove')
+    delButton.textContent = 'delete'
+    delButton.addEventListener('click', () => this.removeMovie(flick, item))
+    actions.appendChild(delButton)
+
+    const favButton = document.createElement('button')
+    favButton.classList.add('fav')
+    favButton.textContent = 'favorite'
+    favButton.addEventListener('click',() => this.toggleFavorite(flick, item))
+    actions.appendChild(favButton)
+
+    return actions
+  }
+
+  renderProperties(flick, item) {
+    const div = document.createElement('div')
+    div.classList.add('info')
 
     const properties = Object.keys(flick)
 
     properties.forEach((propertyName) => {
       const span = this.renderProperty(propertyName, flick[propertyName])
-      item.appendChild(span)
+      div.appendChild(span)
     })
+    return div
+  }
+
+  renderItem(flick) {
+    const item = document.createElement('li')
+    item.classList.add('flick')
+
+    const properties = this.renderProperties(flick, item)
+    item.appendChild(properties)
+
+    const actions = this.renderActionButtons(flick, item)
+    item.appendChild(actions)
 
     return item
+  }
+
+  toggleFavorite(flick, item) {
+    flick.favorite = item.classList.toggle('fav')
+  }
+
+  removeMovie(flick, item) {
+    this.list.removeChild(item)
+
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
   }
 
   handleSubmit(ev) {
@@ -34,13 +78,14 @@ class App {
 
     const flick = {
       name: f.flickName.value,
-      chris: f.whichChris.value,
+      year: f.flickYear.value,
+      favorite: false,
     }
 
+    this.flicks.push(flick)
+
     const item = this.renderItem(flick)
-    this.array.push(flick)
-    const list = document.querySelector('#flicks')
-    list.appendChild(item)
+    this.list.appendChild(item)
 
     f.reset()
     f.flickName.focus()
