74f45e7d1dd323e655214403e1d880ce4d2021aa
Logan Compton <lcompto@iu.edu>
2018-06-28 11:10:46 -0400

    Add each flick into an array

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 class App {
   constructor() {
     const form = document.querySelector('form#flickForm')
+    this.array = []
     form.addEventListener('submit', (ev) => {
       ev.preventDefault()
       this.handleSubmit(ev)
     })
   }
-  
+
   renderProperty(name, value) {
     const span = document.createElement('span')
     span.classList.add(name)
@@ -19,29 +20,31 @@ class App {
     item.classList.add('flick')
 
     const properties = Object.keys(flick)
-  
+
     properties.forEach((propertyName) => {
       const span = this.renderProperty(propertyName, flick[propertyName])
       item.appendChild(span)
     })
+
     return item
   }
 
-
-  submitHandler(ev) {
+  handleSubmit(ev) {
     const f = ev.target
 
     const flick = {
       name: f.flickName.value,
       chris: f.whichChris.value,
     }
-    
-    const item = this.renderItem(flick)
 
+    const item = this.renderItem(flick)
+    this.array.push(flick)
     const list = document.querySelector('#flicks')
     list.appendChild(item)
 
     f.reset()
-    f.flickname.focus()
+    f.flickName.focus()
   }
 }
+
+const app = new App()
